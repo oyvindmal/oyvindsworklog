868192c4103079c24501e6419950cd7fa39b8da0
Øyvind Malin <oyvindmal@outlook.com>
2015-11-26 22:00:38 +0100

    Attempted to add express server

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
+/*var http = require('http')
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port);
+}).listen(port);*/
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 1337;
+app.get('/', function (req, res) {
+  res.send('Hello World from Express!');
+});
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
