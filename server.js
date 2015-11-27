82da9fae9093fa2cfda6bad3aaef39474721224b
Øyvind Malin <oyvindmal@outlook.com>
2015-11-27 02:25:36 +0100

    Removed unused code in server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.set('view engine', 'jade');
 app.get('/', function (req, res) {
   fs.readFile('./content/index.md', 'utf8', function(err, contents) {
       res.render('standardpage', {'markdownhtml': markdown.toHTML(contents)});
-    // res.send(toptemplate + markdown.toHTML(contents) + bottomtemplate);
   });
 
 
@@ -39,17 +38,6 @@ app.use('/img', express.static('img'));
     {
       res.send("not found");
     }
-  /*  if(fs.statSync(filepath) != undefined)
-    {
-      fs.readFile(filepath, 'utf8', function(err, contents) {
-         res.send(markdown.toHTML(contents));
-       });
-    }
-
-    else {
-      res.send("Not found");
-    }*/
-
 });
 
 http.listen(port, function() {
@@ -60,11 +48,3 @@ function getTemplateFile(templatename){
     var path = "./templates/" + templatename + ".html";
     return fs.readFileSync(path);
 }
-/*var server = app.listen(port, function () {
-  var host = server.address().address;
-
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
-*/
-//the end
